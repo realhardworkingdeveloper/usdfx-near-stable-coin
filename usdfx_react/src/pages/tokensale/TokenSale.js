7eb43813edd3392e89f973fbed61979380e2c34e
realhardworkingdeveloper <realhardworkingdeveloper@gmail.com>
2022-07-21 07:00:57 -0700

    update ui/ux

--- usdfx_react/src/pages/tokensale/TokenSale.js
+++ usdfx_react/src/pages/tokensale/TokenSale.js
@@ -14,8 +14,6 @@ const TokenSale = () => {
 
     const navigate = useNavigate();
 
-    console.log(transactionHash);
-
     useEffect(() => {
         if(transactionHash) {
             setShowTransationModal(true);
